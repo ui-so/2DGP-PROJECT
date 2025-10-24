68ad5accf5aadc00c7957d1e5dba750fff9cfdcf
Seo Uiso <uiss@tukorea.ac.kr>
2025-10-25 01:02:23 +0900

    main.py 추가

---
+++ main.py
@@ -0,0 +1,58 @@
+from pico2d import *
+from player import Player
+
+# Game object class here
+
+
+def handle_events():
+    global running
+
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+        else:
+            boy.handle_event(event) # SPACE 들어오면, 소년에게 전달aaa
+
+
+
+def reset_world():
+    global world
+    global boy
+
+    world = []
+
+    player = Player()
+    world.append(boy)
+
+
+
+def update_world():
+    for o in world:
+        o.update()
+    pass
+
+
+def render_world():
+    clear_canvas()
+    for o in world:
+        o.draw()
+    update_canvas()
+
+
+running = True
+
+
+
+open_canvas()
+reset_world()
+# game loop
+while running:
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.01)
+# finalization code
+close_canvas()
