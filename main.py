f99e1f1587a25d6e4028c3d6dd5d45df273a52d8
Seo Uiso <uiss@tukorea.ac.kr>
2025-11-02 21:50:05 +0900

    game_framework 적용되게 수정

--- main.py
+++ main.py
@@ -1,58 +1,7 @@
+import game_framework
 from pico2d import *
-from player import Player
-
-# Game object class here
-
-
-def handle_events():
-    global running
-
-    event_list = get_events()
-    for event in event_list:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
-        else:
-            player.handle_event(event) # SPACE 들어오면, 소년에게 전달aaa
-
-
-
-def reset_world():
-    global world
-    global player
-
-    world = []
-
-    player = Player()
-    world.append(player)
-
-
-
-def update_world():
-    for o in world:
-        o.update()
-    pass
-
-
-def render_world():
-    clear_canvas()
-    for o in world:
-        o.draw()
-    update_canvas()
-
-
-running = True
-
-
+import play_mode as start_mode
 
 open_canvas()
-reset_world()
-# game loop
-while running:
-    handle_events()
-    update_world()
-    render_world()
-    delay(0.01)
-# finalization code
+game_framework.run(start_mode)
 close_canvas()
